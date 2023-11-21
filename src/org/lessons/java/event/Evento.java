dc791baaffb002d365ca62f98e4148f7796568b9
TALEX1995 <tanase.alex@libero.it>
2023-11-21 10:40:50 +0100

    Added Milestone 1

--- src/org/lessons/java/event/Evento.java
+++ src/org/lessons/java/event/Evento.java
@@ -1,6 +1,7 @@
 package org.lessons.java.event;
 
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 public class Evento {
 
@@ -9,7 +10,7 @@ public class Evento {
 	private int totalSeatsNumber;
 	private int reservedSeats;
 	
-	public Evento(String title, LocalDate date, int totalSeatsNumber) {
+	public Evento(String title, LocalDate date, int totalSeatsNumber) throws Exception {
 		setTitle(title);
 		setDate(date);
 		setTotalSeatsNumber(totalSeatsNumber);
@@ -20,23 +21,81 @@ public class Evento {
 	public String getTitle() {
 		return title;
 	}
+	
 	public void setTitle(String title) {
 		this.title = title;
 	}
+	
 	public LocalDate getDate() {
 		return date;
 	}
-	public void setDate(LocalDate date) {
+	
+	public void setDate(LocalDate date) throws Exception {
+//		Check if date is before today
+		LocalDate localDate = LocalDate.now();
+		if(date.isBefore(localDate)) {
+			throw new Exception("Errore: La data dell'evento non può essere antecedente alla data di oggi");
+		}
+			
 		this.date = date;
 	}
+	
 	public int getTotalSeatsNumber() {
 		return totalSeatsNumber;
 	}
-	private void setTotalSeatsNumber(int totalSeatsNumber) {
+	
+	private void setTotalSeatsNumber(int totalSeatsNumber) throws Exception{
+//		Check if total seats is 0 or less
+		if(totalSeatsNumber <= 0) {
+			throw new Exception("Errore: I posti devono essere superiori a zero");
+		}
 		this.totalSeatsNumber = totalSeatsNumber;
 	}
+	
 	public int getReservedSeats() {
 		return reservedSeats;
 	}
+	
+	public void reserve() throws Exception {
+		
+		checkDate("Non puoi prenotare un posto per questo evento perchè si è già concluso");
+		
+//		Check if there are remaining seats
+		if((totalSeatsNumber - reservedSeats) < 1) {
+			throw new Exception("Errore: I posti per questo evento sono stati tutti prenotati");
+		}
+		
+		reservedSeats++;
+	}
+	
+	public void cancelReserve() throws Exception {
+		
+		checkDate("Non puoi cancellare un posto per questo evento perchè si è già concluso");
+		
+//		Check if reserved seats are 0 or less
+		if(reservedSeats < 1) {
+			throw new Exception("Errore: Non risultano prenotazioni per questo evento");
+		}
+		
+		reservedSeats--;
+	}
+	
+	private void checkDate(String message) throws Exception{
+//		Check if Event date is before local date
+		LocalDate localDate = LocalDate.now();
+		if(date.isBefore(localDate)) {
+			throw new Exception("Errore: " + message);
+		}
+	}
+	
+	@Override
+	public String toString() {
+		
+		DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+		String formattedDate = date.format(dateTimeFormatter);
+		
+		return formattedDate + " - " + title;
+	}
+	
 	
 }
--- src/org/lessons/java/event/Main.java
+++ src/org/lessons/java/event/Main.java
@@ -6,6 +6,22 @@ public class Main {
 
 	public static void main(String[] args) {
 		
+		LocalDate b = LocalDate.parse("2024-03-25");
+		try {
+			Evento a = new Evento("sdff", b, 1);
+			
+			a.reserve();
+			a.cancelReserve();
+			a.reserve();
+			a.cancelReserve();
+			System.out.println(a);
+			
+		} catch(Exception e) {
+			System.out.println(e.getMessage());
+		}
+		
+		
+		
 	}
 
 }
